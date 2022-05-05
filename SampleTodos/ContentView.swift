5229b28d3364d6796dc7643a6e9cb2470db0994b
kattan12345 <renoir5d@gmail.com>
2022-05-06 01:57:06 +0900

    Initial Commit

---
+++ SampleTodos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleTodos
+//
+//  Created by 勝田啓一 on 2022/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleTodos/SampleTodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTodosApp.swift
+//  SampleTodos
+//
+//  Created by 勝田啓一 on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SampleTodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
